607cc908fef07c028b07feb4f16a059eb1099500
Daniel Raevsky <raevckyd@mail.ru>
2022-03-06 23:42:25 +0700

    добавлен popup, функции открытия\закрытия

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('input[name="name"]');
+let jobInput = document.querySelector('input[name="status"]');
+
+function openPopup () {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let profileName = document.querySelector('.profile__name');
+    let profileStatus = document.querySelector('.profile__status');
+    profileName.textContent = nameInput.value;
+    profileStatus.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
