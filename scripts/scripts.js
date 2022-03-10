cb83bb6d5ff7b56e45b391ceb45318d8b6f56389
Daniel Raevsky <raevckyd@mail.ru>
2022-03-10 22:32:35 +0700

    Удалены ненужные шрифты, исправлен рид.мд

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,8 +4,12 @@ let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('input[name="name"]');
 let jobInput = document.querySelector('input[name="status"]');
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
 
  function openPopup () {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileStatus.textContent;
     popup.classList.add('popup_opened');
 }
 
@@ -15,17 +19,11 @@ function closePopup () {
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let profileName = document.querySelector('.profile__name');
-    let profileStatus = document.querySelector('.profile__status');
     profileName.textContent = nameInput.value;
     profileStatus.textContent = jobInput.value;
     closePopup ();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', function () {
-    nameInput.value = document.querySelector(".profile__name").textContent;
-    jobInput.value = document.querySelector(".profile__status").textContent;
-    openPopup();
-});
+editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
